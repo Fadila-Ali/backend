cfb0225461eb2053dc65f4cdd7662522f3604b3b
Fadila-Ali <fadilaali@pursuit.org>
2023-01-09 13:57:06 -0500

    remove uuid

--- app.js
+++ app.js
@@ -4,22 +4,11 @@ const router = express.Router();
 const morgan = require("morgan");
 const cors = require("cors");
 const transactions = require("./controllers/transactionsController");
-const uuid = require("uuid");
-console.log(`Here is a test v1 uuid: ${uuid.v1()}`);
-console.log(`Here is a test v4 uuid: ${uuid.v4()}`);
 
 app.use((req, res, next) => {
   console.log("This code runs for every request!");
   next();
 });
-
-const id = uuid.v4();
-// const transactionsPlusId = transactions
-// .map((transaction) => ({
-//   id,
-//   ...transaction,
-// }));
-// console.log(transactionsPlusId);
 
 app.use(express.json());
 app.use(cors());
