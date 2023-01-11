4c8c6f285898e5e93de0e8540432f6d42f35f5c8
Fadila-Ali <fadilaali@pursuit.org>
2023-01-10 23:13:30 -0500

    data updated

--- models/transactions.js
+++ models/transactions.js
@@ -1,26 +1,5 @@
 module.exports = [
   {
-    item_name: "payroll",
-    amount: 3000,
-    date: "2022-12-31",
-    from: "Uber",
-    category: "Earnings",
-  },
-  {
-    item_name: "gym",
-    amount: 25,
-    date: "2023-01-05",
-    from: "Blink",
-    category: "Bills",
-  },
-  {
-    item_name: "car",
-    amount: 250,
-    date: "2023-01-02",
-    from: "Car Guru",
-    category: "Bills",
-  },
-  {
     item_name: "Netflex",
     amount: 20,
     date: "2022-12-15",
@@ -33,5 +12,33 @@ module.exports = [
     date: "2022-12-20",
     from: "Assets",
     category: "Earnings",
+  },
+  {
+    item_name: "payroll",
+    amount: 3000,
+    date: "2022-12-31",
+    from: "Uber",
+    category: "Earnings",
+  },
+  {
+    item_name: "car",
+    amount: 250,
+    date: "2023-01-02",
+    from: "Car Guru",
+    category: "Bills",
+  },
+  {
+    item_name: "gym",
+    amount: 25,
+    date: "2023-01-05",
+    from: "Blink",
+    category: "Bills",
+  },
+  {
+    item_name: "Food",
+    amount: 100,
+    date: "2023-01-07",
+    from: "Restaurant",
+    category: "Bills",
   },
 ];
